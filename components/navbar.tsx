c8b326374dffe0de6cff726f7a5cf22e598c8f90
Py528 <pranaav.shinde5280@gmail.com>
2023-09-01 18:15:09 +0530

    Chatgpt done UI not working

---
+++ components/navbar.tsx
@@ -0,0 +1,31 @@
+import { UserButton } from '@clerk/nextjs';
+import { Button } from './ui/button';
+import { Menu } from 'lucide-react';
+
+import { MobileSidebar } from "@/components/mobile-sidebar";
+// import { getApiLimitCount } from "@/lib/api-limit";
+// import { checkSubscription } from "@/lib/subscription";
+
+const Navbar = async () => {
+	// const apiLimitCount = await getApiLimitCount();
+	// const isPro = await checkSubscription();
+
+	return (
+		<div className="flex items-center p-4">
+			{/* <MobileSidebar isPro={isPro} apiLimitCount={apiLimitCount} /> */}
+			<MobileSidebar />
+			<Button
+				variant="ghost"
+				size="icon"
+				className="md:hidden"
+			>
+				<Menu />
+			</Button>
+			<div className="flex w-full justify-end">
+				<UserButton afterSignOutUrl="/" />
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
